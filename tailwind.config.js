8b316bdf726973bcd5bb9c8d8fb4696989c1b82a
Vitor Samuel Miranda de Souza <Vitorsmiranda31@gmail.com>
2025-06-07 20:43:07 +0200

    Search Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 //tailwind.config.css
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
